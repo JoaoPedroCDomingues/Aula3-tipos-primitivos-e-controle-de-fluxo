58b9848b3a0bb343576e2d43b5a76b6c59137f04
JoaoPedroCDomingues <jpcdomingues2003@gmail.com>
2025-08-06 21:56:00 -0300

    Add files via upload

---
+++ Aula3 - Exercicio1/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int h1;
+        int h2;
+        int h3;
+        int total;
+        int media;
+
+        h1 = 10;
+        h2 = 4;
+        h3 = 2;
+
+        total = h1 + h2 + h3;
+
+        media = (total / 3);
+
+        System.out.println("Total de lanches: " + total);
+        System.out.println("Media de lanches por hora: " + media);
+    }
+}
