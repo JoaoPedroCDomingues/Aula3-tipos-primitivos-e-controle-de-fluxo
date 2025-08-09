48a785a080f44d1a1ff4e1a9788b59331eb07601
JoaoPedroCDomingues <jpcdomingues2003@gmail.com>
2025-08-09 19:09:41 -0300

    Add files via upload

---
+++ Aula3 - Exercicio5/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
+
+        int x = rand.nextInt(10) + 1;
+        int n;
+
+        System.out.println("Adivinhe o numero aleatório entre 1 e 10");
+        do {
+
+            n = sc.nextInt();
+
+            if (n != x){
+                System.out.println("Errado! Tente novamente");
+            }
+            else if(n == x){
+                System.out.println("Parabens! Você acertou!");
+            }
+
+        }while (n != x);
+
+        sc.close();
+
+    }
+}
